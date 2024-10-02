55b01aa0a110b1e8c055c97d0e96aa40b31083ee
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-10-02 13:33:26 -0300

    Mudanças na pagina de Login, padding, imagens e margins

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,30 +1,95 @@
 export default function login() {
-    return (
-        <div className="flex justify-center items-center flex-col gap-5 bg-[#0B0F18] w-screen h-screen">
-                <figure>
-                    <img src="./logo.png" alt="Logo" />
-                </figure>
-                <form className="w-2/6">
-                    <div className="mb-5 w">
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Email</label>
-                        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Sua senha</label>
-                        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-                    </div>
-                    <div className="flex items-start mb-5">
-                        <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 " required />
-                        </div>
-                        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-300">Lembre-me</label>
-                    </div>
-                    <button type="submit" className=" text-white bg-[#B38000] font-bold hover:bg-[#947321] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm w-full px-5 py-2.5 text-center">Entrar</button>
-                    <div className="flex justify-between pt-5">
-                        <a href="#">Registrar-se</a>
-                        <a href="#" >Esqueceu sua senha?</a>
-                    </div>
-                </form>
-            </div>
-    )
+  return (
+    <div className="flex justify-center items-center flex-col gap-5 bg-[#0B0F18] w-screen h-screen">
+      <figure>
+        <img src="./logo.png" alt="Logo" />
+      </figure>
+      <form className="w-2/6">
+        <label
+          htmlFor="input-group-1"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Seu Email:
+        </label>
+        <div className="relative mb-6">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="input-group-1"
+            className=" border text-sm rounded-lg block w-full ps-10 p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <label
+          htmlFor="input-group-1"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Sua Senha:
+        </label>
+        <div className="relative mb-6">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg
+              className="w-6 h-6 text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14v3m-3-6V7a3 3 0 1 1 6 0v4m-8 0h10a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="password"
+            id="input-group-1"
+            className=" border text-sm rounded-lg block w-full ps-10 p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-700 focus:ring-3 focus:ring-blue-300 "
+              required
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-300"
+          >
+            Lembrar-se De Mim
+          </label>
+        </div>
+        <button
+          type="submit"
+          className=" text-black bg-[#B38000] font-bold hover:bg-[#947321] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm w-full px-5 py-2.5 text-center"
+        >
+          Entrar
+        </button>
+        <div className="flex justify-between pt-5">
+          <a href="/" className="font-semibold">Registrar-se</a>
+          <a href="/" className="font-semibold">Esqueceu sua senha?</a>
+        </div>
+      </form>
+    </div>
+  );
 }
