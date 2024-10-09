9322e12257da46f11ebe4875d964b7788e6b4e87
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-10-09 12:58:00 -0300

    Feat: Fix Performance Issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,8 @@ import { useUsuarioStore } from '@/context/usuario';
 import { useRouter } from 'next/navigation';
 import { HiAnnotation } from 'react-icons/hi';
 import { HiUserGroup } from 'react-icons/hi';
-import { HiGlobeAmericas } from 'react-icons/hi2';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { JetBrains_Mono } from 'next/font/google'
+import { JetBrains_Mono } from 'next/font/google';
 
 const jetbrains = JetBrains_Mono({
   weight: ['400', '500', '600', '700'],
@@ -33,7 +32,9 @@ export default function Header() {
           className="flex justify-center items-center flex-col space-x-3 rtl:space-x-reverse"
         >
           <img src="/logos/Logo_preta.png" className="h-10" />
-          <span className={`${jetbrains.className} p-0 pr-2  elf-center text-lg font-semibold whitespace-nowrap text-white`}>
+          <span
+            className={`${jetbrains.className} p-0 pr-2  elf-center text-lg font-semibold whitespace-nowrap text-white`}
+          >
             Verbalize
           </span>
         </a>
@@ -48,7 +49,7 @@ export default function Header() {
                   href="/"
                   className="flex items-center justify-center flex-col font-bold py-2 px-3 text-white text-base rounded  hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#B38000]  md:p-0 md:dark:hover:text-[#B38000]  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  <HiUserGroup size={38} className='text-black' />
+                  <HiUserGroup size={38} className="text-black" />
                   Comunidades
                 </Link>
               </li>
@@ -57,27 +58,23 @@ export default function Header() {
                   href="/"
                   className=" flex items-center justify-center flex-col font-bold py-2 px-3 text-white text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#B38000]  md:p-0 md:dark:hover:text-[#B38000]  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  <HiAnnotation size={38} className='text-black' />
+                  <HiAnnotation size={38} className="text-black" />
                   Mensagens
                 </a>
               </li>
-              <li>
+              <li className="flex items-center justify-center gap-5">
                 <Link
                   href="/perfil"
-                  className="flex items-center mt-2 justify-center flex-col font-bold py-2 px-3 text-white text-lg rounded hover:text-[#B38000] hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#B38000]  md:p-0 md:dark:hover:text-[#B38000]  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="flex items-center justify-center flex-col font-bold py-2 px-3 text-white text-lg rounded hover:text-[#B38000] hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#B38000]  md:p-0 md:dark:hover:text-[#B38000]  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   <Avatar>
                     <AvatarImage src={usuario.foto} />
                     <AvatarFallback>CN</AvatarFallback>
                   </Avatar>
-
-                  {usuario.nome}
                 </Link>
-              </li>
-              <li>
                 <span
                   onClick={sairCliente}
-                  className="cursor-pointer transition delay-150 duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] flex items-center justify-center m-0 text-white bg-slate-800 hover:bg-[#B38000] focus:ring-4 focus:outline-none font-medium rounded-3xl text-sm px-6 py-3 text-center "
+                  className="cursor-pointer transition delay-150 duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] flex items-center justify-center m-0 text-white bg-slate-800 hover:bg-[#B38000] focus:ring-4 focus:outline-none font-medium rounded-3xl text-sm px-4 py-2 text-center "
                 >
                   Sair
                 </span>
@@ -94,7 +91,7 @@ export default function Header() {
                 <li>
                   <Link
                     href="/informacoes"
-                    className="hidden py-2 px-3 text-white text-xs font-bold bg-blue-700 md:bg-transparent md:hover:text-[#B38000]  md:p-0 xl:block lg:text-sm xl:text-base 2xl:text-lg"
+                    className="block py-2 px-3 text-white text-xs font-bold bg-blue-700 md:bg-transparent md:hover:text-[#B38000]  md:p-0 lg:text-sm xl:text-base 2xl:text-lg"
                     aria-current="page"
                   >
                     Porque escolher nós
