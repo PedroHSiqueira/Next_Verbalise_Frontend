da7e07b16d5bc306172cd656bc556e42bacc6cce
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-10-22 20:57:27 -0300

    Add flag to profile

--- src/components/itemLanguage.tsx
+++ src/components/itemLanguage.tsx
@@ -1,20 +1,31 @@
-import { IdiomaUsuarioI } from "@/utils/types/idiomaUsuario";
+import { IdiomaUsuarioI } from '@/utils/types/idiomaUsuario';
+import '/node_modules/flag-icons/css/flag-icons.min.css';
 
-export default function ItemLanguage({data}: {data: IdiomaUsuarioI}) {
-    let lingua;
-    if(data.idiomaId == 3) {
-        lingua = 'Inglês - US';
-    }else if(data.idiomaId == 4) {
-        lingua = 'Português - BR';
-    }else{
-        lingua = 'Aguardando dados';
-    }
+export default function ItemLanguage({ data }: { data: IdiomaUsuarioI }) {
+  let lingua;
+  if (data.idiomaId == 3) {
+    lingua = (
+      <div className='flex items-center justify-center gap-3'>
+        <p>Inglês</p>
+        <span className="fi fi-us"></span>
+      </div>
+    );
+  } else if (data.idiomaId == 4) {
+    lingua = (
+      <div className='flex items-center justify-center gap-3'>
+        <p>Português</p>
+        <span className="fi fi-br"></span>
+      </div>
+    );
+  } else {
+    lingua = 'Aguardando dados';
+  }
 
   return (
     <div className="flex flex-col gap-10 md:flex-row">
       <div>
         <h2 className="bg-white text-center p-4 rounded-3xl shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
-            {lingua}
+          {lingua}
         </h2>
       </div>
     </div>
