7cbc2f32f3f2ffbf1b4e4bf877538366574e7794
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-10-24 19:20:23 -0300

    Fix age

--- src/components/itemLanguage.tsx
+++ src/components/itemLanguage.tsx
@@ -5,7 +5,7 @@ export default function ItemLanguage({ data }: { data: IdiomaUsuarioI }) {
   let lingua;
   if (data.idiomaId == 1) {
     lingua = "Português - BR";
-  } else if (data.idiomaId == 4) {
+  } else if (data.idiomaId == 2) {
     lingua =  "Inglês - US";
   } else {
     lingua = 'Aguardando dados';
@@ -15,7 +15,7 @@ export default function ItemLanguage({ data }: { data: IdiomaUsuarioI }) {
     <div className="flex flex-col gap-10 md:flex-row">
       <div>
         <h2 className="bg-white text-center p-4 rounded-3xl shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
-        {lingua || 'Aguardando'}
+        {lingua}
         </h2>
       </div>
     </div>
