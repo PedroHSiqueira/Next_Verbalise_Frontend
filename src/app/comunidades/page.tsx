7bad8bd4d3b399f175003df5aeb1e47078ef74ba
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-08 23:36:59 -0300

    Feat: Remove the user for system

--- src/app/comunidades/page.tsx
+++ src/app/comunidades/page.tsx
@@ -1,28 +1,25 @@
-'use client';
-import Comunidades from '@/components/BannerComunidade';
-import ItemUsuario from '@/components/cardUsuario';
-import Header from '@/components/header';
-import { useUsuarioStore } from '@/context/usuario';
-import { UsuarioI } from '@/utils/types/usuarios';
-import { useEffect, useState } from 'react';
+"use client";
+import Comunidades from "@/components/BannerComunidade";
+import ItemUsuario from "@/components/cardUsuario";
+import Header from "@/components/header";
+import { useUsuarioStore } from "@/context/usuario";
+import { UsuarioI } from "@/utils/types/usuarios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [usuarios, setUsuarios] = useState<UsuarioI[]>([]);
   const { logar } = useUsuarioStore();
   useEffect(() => {
     async function buscaUsuarios(idUsuario: string) {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_URL_API}/usuarios/conta/${idUsuario}`,
-      );
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/usuarios/conta/${idUsuario}`);
       if (response.status === 200) {
         const dados = await response.json();
         logar(dados);
       }
     }
-    if (localStorage.getItem('client_key')) {
-      const usuarioSalvo = localStorage.getItem('client_key') as string;
-      const usuarioValor = usuarioSalvo.replace(/"/g, '');
+    if (localStorage.getItem("client_key")) {
+      const usuarioSalvo = localStorage.getItem("client_key") as string;
+      const usuarioValor = usuarioSalvo.replace(/"/g, "");
       buscaUsuarios(usuarioValor);
     }
 
@@ -37,7 +34,9 @@ export default function Home() {
     }
     getUsuarios();
   }, []);
-  let listaUsuarios = usuarios.map((usuario) => <ItemUsuario key={usuario.id} data={usuario} />);
+
+  const usuarioLogado = localStorage.getItem("client_key")?.replace(/"/g, "");
+  let listaUsuarios = usuarios.filter((usuario) => usuario.id !== usuarioLogado).map((usuario) => <ItemUsuario key={usuario.id} data={usuario} />);
 
   return (
     <div>
@@ -48,9 +47,7 @@ export default function Home() {
         <section>
           <Comunidades />
         </section>
-        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-6'>
-        {listaUsuarios}
-        </section>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-6">{listaUsuarios}</section>
       </main>
     </div>
   );
