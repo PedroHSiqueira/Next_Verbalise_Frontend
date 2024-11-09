c874ea6411f7fa9e0e827e310e0c47c84aeaf7a1
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-09 01:29:05 -0300

    Fix: Profile Details

--- src/components/cardUsuario.tsx
+++ src/components/cardUsuario.tsx
@@ -1,61 +1,43 @@
 import { UsuarioI } from "@/utils/types/usuarios";
-
-
-
+import Link from "next/link";
 
 export default function ItemUsuario({ data }: { data: UsuarioI }) {
-    
-    function calculaIdade(dataNascimento: string) {
-        const dataAtual = new Date();
-        const dataNasc = new Date(dataNascimento);
-        let idade = dataAtual.getFullYear() - dataNasc.getFullYear();
-        const mes = dataAtual.getMonth() - dataNasc.getMonth();
-        if (mes < 0 || (mes === 0 && dataAtual.getDate() < dataNasc.getDate())) {
-            idade -= 1;
-        }
-        return idade;
+  function calculaIdade(dataNascimento: string) {
+    const dataAtual = new Date();
+    const dataNasc = new Date(dataNascimento);
+    let idade = dataAtual.getFullYear() - dataNasc.getFullYear();
+    const mes = dataAtual.getMonth() - dataNasc.getMonth();
+    if (mes < 0 || (mes === 0 && dataAtual.getDate() < dataNasc.getDate())) {
+      idade -= 1;
     }
-    const idadeConvertida = Math.abs(calculaIdade(data.nascimento));
+    return idade;
+  }
+  const idadeConvertida = Math.abs(calculaIdade(data.nascimento));
 
-    return (
-
-        
-            <div className="border rounded-lg max-w-[506px] max-h-[266px] p-4 shadow bg-[#f3efe5]">
-                <div className="flex items-center mb-4">
-                    <img
-                        className="w-16 h-16 rounded-full"
-                        src={data.foto}
-                        alt="Foto"
-                    />
-                    <div className="ml-4">
-                        <h2 className="text-xl font-semibold">
-                            {data.nome} {idadeConvertida}
-                        </h2>
-                        <p className="text-sm">Nacionalidade: {data.nacionalidade}</p>
-                    </div>
-                </div>
-                <p className="text-gray-600 text-sm mb-4">
-                    {data.descricao}
-                </p>
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center">
-                        <span className="mr-2">Fala: </span>
-                        <img
-                            className="w-8 h-8"
-                            src="./Bandeiras/france.png"
-                            alt="França"
-                        />
-                    </div>
-                    <div className="flex items-center">
-                        <span className="mr-2">Aprende:</span>
-                        <img
-                            className="w-8 h-8"
-                            src="./Bandeiras/brazil.png"
-                            alt="Brasil"
-                        />
-                    </div>
-                </div>
+  return (
+    <Link href={`/detalhes/${data.id}`}>
+        <div className="border rounded-lg max-w-[506px] max-h-[266px] p-4 shadow bg-[#f3efe5]">
+          <div className="flex items-center mb-4">
+            <img className="w-16 h-16 rounded-full" src={data.foto} alt="Foto" />
+            <div className="ml-4">
+              <h2 className="text-xl font-semibold">
+                {data.nome} {idadeConvertida}
+              </h2>
+              <p className="text-sm">Nacionalidade: {data.nacionalidade}</p>
             </div>
-        
-    );
-};
+          </div>
+          <p className="text-gray-600 text-sm mb-4">{data.descricao}</p>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <span className="mr-2">Fala: </span>
+              <img className="w-8 h-8" src="./Bandeiras/france.png" alt="França" />
+            </div>
+            <div className="flex items-center">
+              <span className="mr-2">Aprende:</span>
+              <img className="w-8 h-8" src="./Bandeiras/brazil.png" alt="Brasil" />
+            </div>
+          </div>
+        </div>
+    </Link>
+  );
+}
