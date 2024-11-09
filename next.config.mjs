2920fd6171b11341184acbfd8676206d0cfae9ad
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-08 23:10:02 -0300

    Feat: Add better styles in photo input

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "encrypted-tbn0.gstatic.com"
-            },
-            {
-                protocol: "https",
-                hostname: "files.tecnoblog.net"
-            },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "files.tecnoblog.net",
+      },
 
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com"
-            }
-        ]
-    },
-    "reactStrictMode": false,
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
